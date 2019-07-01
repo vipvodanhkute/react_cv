65b960050c088c704951e138220f04698ddadb77
Nhu Huynh <vipvodanhmaster@gmail.com>
2019-07-01 10:30:22 +0700

    begin

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Login from './components/login';
 import Info from './components/info';
 import Javascript from './components/javascript';
 import Header from './components/header';
 import '../public/css/style.css'
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      isLogin:'login',
-      isHeader:false
-    }
-    this.onStatus=this.onStatus.bind(this)
-    this.isHeader=this.isHeader.bind(this)
-  } 
-  isHeader(){
-    this.setState({isHeader:!this.state.isHeader})
-  }
-  onStatus(status){
-    this.setState({isLogin:status})
-  }
-  onRender(){
-    switch(this.state.isLogin){
-      case 'login':
-        return <Login status={this.onStatus} header={this.isHeader}/>
-        break;
-      case 'info':
-        return <Info status={this.onStatus}/>
-        break;
-      default :
-        return <Javascript status={this.onStatus}/>
-        break;
-    }
-  }
   render() {
     return (
-      <div>
-        {this.state.isHeader?<Header header={this.isHeader} status={this.onStatus}/>:''}
-         {this.onRender()}
-      </div>
+      <Router>
+        <Route path="/" component={Login} />
+      </Router>
     );
   }
 }
